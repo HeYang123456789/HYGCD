9725aa83d311d94cfee5ac159c38438cd81a759d
HeYang123456789 <1554566348@qq.com>
2016-03-16 19:05:03 +0800

    Objective-C的GCD接口(自主封装)

---
+++ HYGCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYGCD
+//
+//  Created by HEYANG on 16/3/16.
+//  Copyright © 2016年 HEYANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYGCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYGCD
+//
+//  Created by HEYANG on 16/3/16.
+//  Copyright © 2016年 HEYANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
